02f65889fa96bdd3dd16c08e08031d012be1a10d
safrain <z.safrain@gmail.com>
2014-05-03 01:27:04 +0800

    Link with sae

--- pageshot.groovy
+++ pageshot.groovy
@@ -6,12 +6,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
 
-def commands = [
-	['test1', 'ps aux'],
-	['test2', 'ssh -I'],
-	['test3', 'netstat -ntpl'],
-]
+
+def MAX_COMMAND_LENGTH = 40
+
+def MAX_EXPLAIN_LENGTH = 300
+
+
+
+def http = new HTTPBuilder('http://4.commandlinefu.sinaapp.com')
+
+// [[id, command],...]
+def commands =  http.request(GET, TEXT) {
+	uri.path = '/explain_task.php'
+	uri.query = [:]
+	headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+
+	response.success = { resp, content ->
+		new groovy.json.JsonSlurper().parseText content.text
+	}
+	
+	response.failure = { resp ->
+	  println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+	}
+}.collect{ [ it.id,it.command ]}
 
 def TEMPLATE = new groovy.text.SimpleTemplateEngine().createTemplate(new File('template.html').getText('UTF-8'))
 def p = '/usr/bin/Xvfb :99'.execute();
@@ -24,9 +45,18 @@ def tmp = File.createTempFile('explainImage','')
 tmp.deleteOnExit()
 
 commands.each {
+	if(it[1].length() > MAX_COMMAND_LENGTH){
+		println "${it[0]} IGNORE"
+		return
+	}
 	def ep = "python explain.py".execute()
 	ep.withWriter { w-> w << it[1] }
-	new groovy.json.JsonSlurper().parseText(ep.in.text).with { json ->
+	def text = ep.in.text
+	if(!text) {
+		println "${it[0]} ERROR PYTHON"
+		return
+	}
+	new groovy.json.JsonSlurper().parseText(text).with { json ->
 		TEMPLATE.make(
 		        [
 		                command: json.command,
@@ -38,13 +68,20 @@ commands.each {
 		                                text: it.match,
 		                                explain: json.helptext.find { exp -> exp[1] == it.helpclass }
 		                        ]
-		                }.findAll { it.explain }.collect { it.explain = it.explain[0]; it }//.findAll { it.explain.length() < 30000 }
+		                }.findAll { it.explain }.collect { it.explain = it.explain[0]; it }.findAll { it.explain.length() < MAX_EXPLAIN_LENGTH }
 		        ]
 		).writeTo(tmp.newWriter())
+		true
 	}
 	driver.get("file://${tmp}")	
-	new File("${it[0]}.png").bytes = driver.getScreenshotAs(OutputType.BYTES)
-	println "${it[0]} OK"
+//	new File("${it[0]}.png").bytes = 
+	http.request(POST) { h ->
+		uri.path = "/uploadExplain.php?id=${it[0]}"
+		body = driver.getScreenshotAs(OutputType.BYTES)
+		requestContentType = ContentType.BINARY
+		response.success = { resp -> println "${it[0]} OK" }
+		response.failure = { resp -> println "${it[0]} UPLOAD ERROR: ${resp.statusLine}" }
+	}
 }
 driver.close()
 p.destroy()
